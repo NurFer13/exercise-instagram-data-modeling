06abb5630f36ae8da1c47ae04745d98041d10496
NurFer13 <nurfer1307@gmail.com>
2023-04-18 17:40:35 +0000

    finished

--- src/models.py
+++ src/models.py
@@ -7,23 +7,53 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    username = Column(String(250),unique=True, nullable=False)
+    firstname = Column(String(200),unique=True,nullable=False)
+    lastname = Column(String(250),nullable=False)
+    email = Column(String(200),nullable=False)
+    follower = relationship('Follower',back_populates='user')
 
-class Address(Base):
-    __tablename__ = 'address'
+
+class Follower(Base):
+    __tablename__ = 'follower'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    user_id = Column(Integer, ForeignKey('person.id'))
+    user = relationship('User',back_populates='follower')
+
+class Comment(Base):
+    __tablename__ = 'comment'
+    id = Column(Integer, primary_key=True)
+    comment_text = Column(String(500),nullable=True)
+    author_id = Column(Integer,primary_key=True)
+    post_id = Column(Integer,primary_key=True)
+    
+
+class Post(Base):
+    __tablename__ = 'post'  
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('person.id'))
+    media = relationship('Media',back_populates='post')
+
+
+
+class Media(Base):
+    __tablename__ = 'media'
+    id = Column(Integer, primary_key=True)
+    type = Column(enumerate(),nullable=False)
+    url = Column(String(300),unique=True,nullable=False)
+    post_id = Column(Integer,ForeignKey('post_id'))
+    post = relationship('Post',back_populates='media')
+    
+
+    
+
 
     def to_dict(self):
         return {}
